6cd3c47ab69eea94add39df36de967e3fbc04a8f
Eric Truong <erictruong15@gmail.com>
2022-07-12 11:58:09 -0700

    edit nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,12 +8,18 @@ export default function NavBar() {
         <>
             <nav className="nav">
                 <div className="logo-title">
-                    <img alt="logo" src={logo} width="130" height="80"></img>{' '} 
-                    <h1>Shiba Shufflers</h1>
+                    <img alt="logo" src={logo} width="130" height="80"></img>
+                    <h1>
+                        <a href="#home">Shiba Shufflers</a>
+                    </h1>
                 </div>
                 <ul>
-                    <li>About</li>
-                    <li>Schedule</li>
+                    <li>
+                        <a href="#about">About</a>
+                    </li>
+                    <li>
+                        <a href="#schedule">Schedule</a>
+                    </li>
                 </ul>
             </nav>
         </>
